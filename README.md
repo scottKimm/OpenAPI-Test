8b827f0331540886c46ea2ee815c1560627e9767
scottkimm <scott.kim@swit.io>
2023-02-06 10:33:27 +0900

    webhook 예시 및 README 수정

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # OpenAPI-Test
+강의 후 실습 및 테스틀 위해서 작성되었습니다.
 
-flask --debug --app app run
+OAuth 2.0 테스트 시 플라스크 실행 코드
+~~~python
+flask --debug --app oauth run --port 5003
+~~~
+
+webhook 테스트 시 플라스크 실행 코드
+~~~python
+flask --debug --app webhook run --port 5003
+~~~
+
+이 후 ngrok 으로 외부 접속 준비
+(mac 환경에서 5000 포트가 안되는 경우가 있어서 5003 으로 변경)
--- webhook.py
+++ webhook.py
@@ -1,13 +1,35 @@
-from flask import Flask, request
-from urllib import parse
+from flask import Flask
+from flask import request
+
 import requests
+
 app = Flask(__name__)
 
+# slack webhook test
 @app.route("/webhook", methods=['POST'])
 def webhook():
+    # 앞에 필요없는 부분 슬라이스 (슬랙쪽 설정에 맞춰서 한 것이기 때문에 실제 구현때는 적절히 바꿀필요있음)
+    text = request.form.get("text")[7:]
 
-    print(request.form)
-    return {
-        "text": "ok"
+    # swit webhook endpoint
+    endpoint = "https://xxx.xxx/xxx"
+
+    # plain text 만 가능
+    json_data = {
+        "text": text
     }
 
+    headers_data = {
+        'Content-type' : 'application/json'
+    }
+    res = requests.post(url=endpoint, json=json_data, headers=headers_data, timeout=180)
+
+    if res.ok:
+        # slack webhook 이용시 응답
+        response_text = "success"
+
+    response_text = "fail"
+
+    return {
+        "text": response_text
+    }
